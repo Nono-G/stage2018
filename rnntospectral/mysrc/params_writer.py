19d6bc5fde1fc94d7431ef92101261d86ab86cc7
Nono-G <goudian.noe@laposte.net>
2018-04-05 13:15:15 +0200

    adaptation paramswriter a l'option mode de train2f

--- rnntospectral/mysrc/params_writer.py
+++ rnntospectral/mysrc/params_writer.py
@@ -6,9 +6,9 @@ all_neurons = [30, 50, 120]
 all_epochs = [50]
 all_batch = [32]
 all_sample = [75000]
-all_name = ["4Avr"]
+all_name = ["5Avr"]
 all_layer = [3]
-
+all_modes = [0]
 
 for (trainf, testf) in all_probs:
     for wid in all_wid:
@@ -18,5 +18,7 @@ for (trainf, testf) in all_probs:
                     for sample in all_sample:
                         for name in all_name:
                             for layer in all_layer:
-                                print("{0} {1} {2} {3} {4} {5} {6} {7} {8}"
-                                      .format(wid, trainf, neurons, epochs, batch, sample, name, testf, layer))
+                                for mode in all_modes:
+                                    print("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}"
+                                          .format(wid, trainf, neurons, epochs,
+                                                  batch, sample, name, testf, layer, mode))
